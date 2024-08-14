2a1b8fcac5968bbb60901b02f5cac9eaf14d873d
fujisatoshi <464310291@qq.com>
2024-08-14 15:28:52 +0800

    yyyupdate

--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -21,6 +21,9 @@ public class SimpleCalculator {
                     result = Double.POSITIVE_INFINITY; // 或者返回一个错误码
                 }
                 break;
+            case '%':
+                    result = num1 % num2;
+                break;
             default:
                 System.out.println("无效的运算符");
                 break;
@@ -33,7 +36,7 @@ public class SimpleCalculator {
         Scanner scanner = new Scanner(System.in);
         System.out.print("请输入第一个数字: ");
         double num1 = scanner.nextDouble();
-        System.out.print("请输入运算符 (+, -, *, /): ");
+        System.out.print("请输入运算符 (+, -, *, /, %): ");
         String operator = scanner.next();
         System.out.print("请输入第二个数字: ");
         double num2 = scanner.nextDouble();
@@ -56,6 +59,9 @@ public class SimpleCalculator {
                     System.out.println("除数不能为0");
                     return;
                 }
+                break;
+            case "%":
+                    result = num1 % num2;
                 break;
             default:
                 System.out.println("无效的运算符");
